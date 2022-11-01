37b1b9d432f8093f87d37ca463b7b61d34cf6464
similicious <mail@similicious.de>
2022-11-01 12:50:25 +0100

    Added config validation and warnings

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const getAvailableAppointments = require('./get-available-appointments');
 const getBookingPageHtml = require('./get-booking-page-html');
 const sendTelegramNotification = require('./send-telegram-notification');
 
+// Validate configuration values in .env
+const telegramNotificationsEnabled = process.env.TELEGRAM_BOT_TOKEN && process.env.TELEGRAM_CHAT_ID
+validateConfig();
+
 // Setup simple scheduler
 const scheduler = new ToadScheduler()
 const checkForAppointmentsTask = new AsyncTask('checkForAppointments', checkForAppointments, handleErrors)
@@ -18,7 +22,7 @@ async function checkForAppointments() {
   let bookingPageHtml = await getBookingPageHtml();
   const dates = getAvailableAppointments(bookingPageHtml);
 
-  if (dates.length > 0) {
+  if (dates.length > 0 && telegramNotificationsEnabled) {
     const message = `Buergeramt appointments are available now! Check ${process.env.BOOKING_URL}`
     await sendTelegramNotification(message)
   }
@@ -34,5 +38,27 @@ async function checkForAppointments() {
 
 async function handleErrors(err) {
   console.error(err);
-  await sendTelegramNotification(JSON.stringify(err));
+  if(telegramNotificationsEnabled) {
+    await sendTelegramNotification(JSON.stringify(err));
+  }  
+}
+
+function validateConfig() {
+  const { BOOKING_URL, USER_AGENT, CHECK_INTERVAL_MINUTES, HEALTHCHECKS_IO_TOKEN, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID  } = process.env;
+  if (!BOOKING_URL | !CHECK_INTERVAL_MINUTES) {
+    console.error('BOOKING_URL or CHECK_INTERVAL_MINUTES have not been set. Please set values in .env according to README.');
+    process.exit(1);
+  }
+
+  if(!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
+    console.warn('TELEGRAM_BOT_TOKEN or TELEGRAM_CHAT_ID has not been set. You will receive no notifications for appointments.');
+  }
+
+  if (!USER_AGENT) {
+    console.warn('USER_AGENT has not been set. Please add contact information.');
+  }
+
+  if (!HEALTHCHECKS_IO_TOKEN) {
+    console.info('HEALTHCHECKS_IO_TOKEN has not been set. The script execution will not be monitored.')
+  }
 }
